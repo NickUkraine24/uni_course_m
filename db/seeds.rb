f4a311c6fb56bdd4403c63ed25d77ef6dd55ae0f
Mykola Ilashchuk <80954980827ni@gmail.com>
2022-11-22 09:52:58 +0200

    Added `has_many :courses` and `belongs_to :user` associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+User.create!(email: "test@test.com", password: "123456", password_confirmation: "123456")
 
 30.times do
   Course.create!([{
     title: Faker::DcComics.title,
-    description: Faker::Quote.matz
+    description: Faker::Quote.matz,
+    user_id: User.first.id
   }])
 end
